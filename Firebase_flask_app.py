2bd1aa9d7680eacbfc0b32ff5faf7af9061a1372
Soumi7 <soumibardhan10@gmail.com>
2021-12-02 13:08:37 +0530

    update

---
+++ Firebase_flask_app.py
@@ -0,0 +1,22 @@
+
+# first will select collection
+
+# then will select document 
+
+# then document
+
+# then document
+
+# then document
+
+# function to get the data from firebase
+
+# display existing fields  (field name : value)
+
+# now update fields
+
+# Add fields 
+
+# For specific field, eg image, add button for upload, function to generate url and add it to field
+
+# For mux_video_url field, add video upload button, function to upload the video to firebase and generate mux link to cahched asset
--- firebase_script.py
+++ firebase_script.py
@@ -1,6 +1,9 @@
+from __future__ import unicode_literals
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import os
+os.environ['GRPC_DNS_RESOLVER'] = 'native'
 
 # Use the application default credentials
 #cred = credentials.Certificate("/mnt/c/Users/soumi/Downloads/fir-flasksofia-firebase-adminsdk-zq0sm-c1b8842fd6.json")
@@ -8,15 +11,46 @@ from firebase_admin import firestore
 
 #cred = credentials.Certificate("/mnt/c/Users/soumi/Downloads/fir-flasksofia-firebase-adminsdk-zq0sm-c1b8842fd6.json")
 
+def make_unicode(inp):
+    if type(inp) != unicode:
+        inp =  inp.decode('utf-8')
+    return inp
+
 cred = credentials.Certificate("fir-flasksofia-firebase-adminsdk-zq0sm-c1b8842fd6.json")
 firebase_admin.initialize_app(cred)
 
 
 db = firestore.client()
 
-doc_ref = db.collection(u'test').document(u'alovelace')
-doc_ref.set({
-    u'first': u'Ada',
-    u'last': u'Lovelace',
-    u'born': 1815
-})
+tracks = db.collection(u'sofia').document(u'test').collection(u'tracks')
+
+track = input("Enter the track")
+
+trackname = f'{track}'
+
+#track_name = u'track_name'
+
+print(trackname)
+print(str(tracks.document(str(trackname).decode('utf8')).collection(u'poses')))
+
+for k in tracks.document(track_name).collection(u'poses').get():
+    print(k)
+
+
+# frank_ref.set({
+#     u'name': u'Frank',
+#     u'favorites': {
+#         u'food': u'Pizza',
+#         u'color': u'Blue',
+#         u'subject': u'Recess'
+#     },
+#     u'age': 12
+# })
+
+#Update age and favorite color
+# frank_ref.update({
+#     u'age': 13,
+#     u'favorites.color': u'Red'
+# })
+
+
